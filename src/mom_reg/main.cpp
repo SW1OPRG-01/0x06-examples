f2d0954d4a91c7d1296618b32c8c8bd0644d8abd
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-10-06 12:05:09 +0200

    Add example code

---
+++ src/mom_reg/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "register.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+  cout << "Welcome to the Ministry of Magic Database" << endl;
+
+  return 0;
+}
