26680060121fc66d8be97c2bb08cd05c29e37fdc
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-10-09 19:13:59 +0200

    Add code example

--- src/structs/no_struct.cpp
+++ src/structs/no_struct.cpp
@@ -1,5 +1,15 @@
+#include <cmath>
 #include <iostream>
 
+using namespace std;
+
+double length(int x, int y);
+
 int main (int argc, char *argv[]) {
+  cout << length(-2,-5) << endl;
   return 0;
+}
+
+double length(int x, int y) {
+  return abs(sqrt(pow(x, 2) + pow(y, 2)));
 }
