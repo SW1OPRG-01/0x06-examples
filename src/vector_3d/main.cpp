1ca5248cfee53708f9c3d9dbc00b63eadf4d8f50
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-10-09 19:40:52 +0200

    Add example

---
+++ src/vector_3d/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "vector3d.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  Vector3D point = { -2, -5, 3 };
+  double value = point.length();
+  cout << point.length() << endl;
+  return 0;
+}
